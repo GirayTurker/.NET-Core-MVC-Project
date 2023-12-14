45c17f34428d9e8dc8351fe799ba1af569cc136b
GirayTurker <tug88418@temple.edu>
2023-12-14 15:07:17 -0500

    EntityFramework Package and Connection to Local SQL databse

---
+++ BulkProject/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkProject.Models
+{
+    public class Category
+    {
+        [Key] //Defines Primary Key for Entity Framework
+        public int Id { get; set; }
+        [Required] //Defines Required Fields for Entity Framework
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
