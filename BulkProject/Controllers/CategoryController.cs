6c0fa7aa4c4d748587ec52611437c6e3cbed3c43
GirayTurker <tug88418@temple.edu>
2023-12-14 22:44:47 -0500

    Initialize CategoryController with Db

--- BulkProject/Controllers/CategoryController.cs
+++ BulkProject/Controllers/CategoryController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkProject.Data;
+using BulkProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkProject.Controllers
 {
     public class CategoryController : Controller
     {
+        //Readonly database object variable. 
+        //No object created. Already created under Program.cs / Add services (Dependency Injection)
+        private readonly ApplicationDbContext _db;
+        //Retrive data from database to show in page
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> categoryListObj = _db.Categories.ToList();
             return View();
         }
     }
