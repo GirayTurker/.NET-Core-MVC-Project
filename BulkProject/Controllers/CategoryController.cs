af652d0df006a0029c946e8b9b8d7d2d627388a8
GirayTurker <tug88418@temple.edu>
2023-12-17 02:53:01 -0500

    Update Category Added

--- BulkProject/Controllers/CategoryController.cs
+++ BulkProject/Controllers/CategoryController.cs
@@ -60,5 +60,58 @@ namespace BulkProject.Controllers
             return View();
             
         }
+
+        //New action method for new category edit
+        public IActionResult EditCategory(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Retrive category by Id from database to edit
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //other ways to retrive category by Id from database to edit
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            //Check if corresponding retrive value is null
+            if (categoryFromDb == null) 
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Invoked when posted with Edit through http
+        //Create new Category Object
+        [HttpPost]
+        public IActionResult EditCategory(Category obj)
+        {
+            //Custom validations for Name Field
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order can not be same as Name");
+            }
+            else if (int.TryParse(obj.Name, out int intValue))
+            {
+                ModelState.AddModelError("Name", "Name can not be a number");
+            }
+
+            //Check all validations 
+            if (ModelState.IsValid)
+            {
+                //Point to SQL database to add created obj
+                _db.Categories.Add(obj);
+                //Add and save it to SQL database
+                _db.SaveChanges();
+                //Redirect to View to category Index page
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
     }
 }
